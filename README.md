f7f4525e3e51e68f6ef430cd5f7670c3f491e5bb
Peter Salvi <vukung@yahoo.com>
2023-12-01 11:12:01 +0100

    Day 1

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 To enhance its "retro" feel, I decided to solve this year's
 problems on an old laptop (an AsusTek 1000HE, single core 1.6
 GHz CPU, 800 MB RAM), with a 32-bit Alpine Linux installed, but
-without X11 - the only tools used are `links` (to access the
-problems), `vim` (to write the code), `tcl` (to run the code),
-and `rlwrap` (for a bit of comfort in `tclsh`).
+without X11 - only in text mode. Not 80x25 though, but 102x33,
+using the 18pt bold Terminus console font.
+
+The only tools used are `links` (to access the problems), `vim`
+(to write the code), `tcl` (to run the code), and `rlwrap` (for
+a bit of comfort in `tclsh`).
---
+++ adv01.tcl
@@ -0,0 +1,49 @@
+set part2 false
+
+# Converts `str` to a digit if starts with one
+# (either in digit or string form in Part 2).
+# Returns -1 otherwise.
+proc toDigit {str} {
+    global part2
+    set d [string index $str 0]
+    if {[string is digit $d]} {
+        return $d
+    }
+    if {$part2} {
+        set numbers {zero one two three four five six seven eight nine}
+        for {set i 0} {$i < 10} {incr i} {
+            set nstr [lindex $numbers $i]
+            if {[string match $nstr* $str]} {
+                return $i
+            }
+        }
+    }
+    return -1
+}
+
+# Finds the first and last digits on a line,
+# and creates a two-digit number as a result.
+proc extractNumber {line} {
+    set n [string length $line]
+    set digits {}
+    for {set i 0} {$i < $n} {incr i} {
+        set d [toDigit [string range $line $i end]]
+        if {$d >= 0} {
+            lappend digits $d
+        }
+    }
+    expr {[lindex $digits 0] * 10 + [lindex $digits end]}
+}
+
+set f [open adv01.txt]
+set sum 0
+while true {
+    set line [gets $f]
+    if {[eof $f]} {
+        close $f
+        break
+    }
+    incr sum [extractNumber $line]
+}
+puts $sum
+
