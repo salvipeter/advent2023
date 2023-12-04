3ca34aae9bcee64d3048cd653ee413d039429547
Peter Salvi <vukung@yahoo.com>
2023-12-04 08:20:34 +0100

    Day 4

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Advent of Code 2023 Solutions
 
 To enhance its "retro" feel, I decided to solve this year's
-problems on an old laptop (an AsusTek 1000HE, single core 1.6
-GHz CPU, 800 MB RAM), with a 32-bit Alpine Linux installed, but
-without X11 - only in text mode. Not 80x25 though, but 102x33,
-using the 18pt bold Terminus console font.
+problems on a 15-year-old old laptop (an AsusTek 1000HE, single
+core 1.6 GHz CPU, 800 MB RAM), with a 32-bit Alpine Linux
+installed, but without X11 - only in text mode. Not 80x25
+though, but 102x33, using the 18pt bold Terminus console font.
 
 The only tools used are `links` (to access the problems), `vim`
 (to write the code), `tcl` (to run the code), and `rlwrap` (for
---
+++ adv04.tcl
@@ -0,0 +1,41 @@
+set part2 false
+
+set f [open adv04.txt]
+set allcards [split [read -nonewline $f] "\n"]
+close $f
+
+set cache [dict create]
+
+proc win {i} {
+    global allcards cache part2
+    if {[dict exists $cache $i]} {
+        return [dict get $cache $i]
+    }
+    set line [lindex $allcards $i]
+    set wins [lrange $line 2 11]
+    set cards [lrange $line 13 end]
+    set count 0
+    foreach c $cards {
+        if {[lsearch -integer $wins $c] >= 0} {
+            incr count
+        }
+    }
+    if {$part2} {
+        set sum 0
+        for {set j 1} {$j <= $count} {incr j} {
+            incr sum [win [expr {$i+$j}]]
+        }
+        set result [expr {1+$sum}]
+    } else {
+        set result [expr {$count > 0 ? 1<<($count-1) : 0}]
+    }
+    dict set cache $i $result
+    return $result
+}
+
+set sum 0
+for {set i 0} {$i < [llength $allcards]} {incr i} {
+    incr sum [win $i]
+}
+puts $sum
+
