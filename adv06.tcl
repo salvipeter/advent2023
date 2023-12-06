09384cb0e152371e4ed599ae8a890f49df25bc02
Peter Salvi <vukung@yahoo.com>
2023-12-06 06:20:27 +0100

    Day 6

---
+++ adv06.tcl
@@ -0,0 +1,18 @@
+proc waysToWin {t d} {
+    # x^2 - t x + d = 0
+    set D [expr {sqrt($t*$t-4*$d)}]
+    set x1 [expr {int(($t-$D)/2)}]
+    set x2 [expr {int(($t+$D)/2)}]
+    expr {$x2-$x1}
+}
+
+set f [open adv06.txt]
+set times [lrange [gets $f] 1 end]
+set distances [lrange [gets $f] 1 end]
+set result 1
+foreach t $times d $distances {
+    set result [expr {$result*[waysToWin $t $d]}]
+}
+puts $result
+puts [waysToWin [join $times {}] [join $distances {}]]
+
