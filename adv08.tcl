6d5f20b8d583bc334325d9b3893bc6b17b3d07f0
Peter Salvi <vukung@yahoo.com>
2023-12-08 06:36:16 +0100

    Day 8

---
+++ adv08.tcl
@@ -0,0 +1,51 @@
+proc step {from count} {
+    global map moves
+    set len [string length $moves]
+    set move [string index $moves [expr {$count % $len}]]
+    lindex [dict get $map $from] [expr {$move=="L" ? 0 : 1}]
+}
+
+proc gcd {a b} {
+    while {$b != 0} {
+        set b [expr {$a % [set a $b]}]
+    }
+    return $a
+}
+
+proc lcm {a b} {expr {$a*$b/[gcd $a $b]}}
+
+set f [open adv08.txt]
+set moves [gets $f]
+gets $f; # empty line
+set map [dict create]
+while true {
+    set line [gets $f]
+    if {[eof $f]} {
+        close $f
+        break
+    }
+    set pair [list [string range $line 7 9] [string range $line 12 14]]
+    dict set map [string range $line 0 2] $pair
+}
+
+# Implicit assumption: after the first ..Z each path cycles back
+foreach from [dict keys $map] {
+    if {[string index $from 2] != "A"} {
+        continue
+    }
+    set count 0
+    set pos $from
+    while {[string index $pos 2] != "Z"} {
+        set pos [step $pos $count]
+        incr count
+    }
+    set path($from) $count
+}
+
+set len 1
+foreach pos [array names path] {
+    set len [lcm $len $path($pos)]
+}
+puts "$path(AAA), $len"
+
+
